f4d8ab3c4c65834710655c119753e18dfb43a49b
jishnuaji <www.jishnu136@gmail.com>
2025-07-28 09:58:12 +0530

    Scripts
    Scripts edits

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void cards(int sloats)
+    {
+        switch (sloats)
+        {
+            case 1:if (sloats == 1)
+                {
+                    Debug.Log("destroy");
+                }
+            break;
+        }
+    }
+
+}
