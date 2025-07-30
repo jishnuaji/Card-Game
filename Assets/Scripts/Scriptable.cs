19193ed680a9303960f4c36007c8550d16d8807f
jishnuaji <www.jishnu136@gmail.com>
2025-07-30 22:45:38 +0530

    Scripts and UI
    Edits

---
+++ Assets/Scripts/Scriptable.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "NewCard", menuName = "Card")]
+public class CardScriptableobject : ScriptableObject
+{
+
+    public string CardName;
+}
--- Assets/Scripts/Second.cs
+++ Assets/Scripts/Second.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems; // Required for IPointerClickHandler
 
-public class Second : MonoBehaviour
+public class Card : MonoBehaviour, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public CardScriptableobject card;
+    public bool clicked = false;
+    public Animator AnimatorControler;
+
+    private void Start()
     {
-        
+        AnimatorControler = gameObject.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerClick(PointerEventData eventData)
     {
-        
+        Debug.Log("Card clicked");
+
+        if (!GameManger.instance.Checking)
+        {
+            if (!clicked)
+            {
+                GameManger.instance.clickCount++;
+                AnimatorControler.SetBool("on", true);
+
+                if (GameManger.instance.clickCount % 2 != 0)
+                {
+                    clicked = true;
+                    GameManger.instance.one = gameObject;
+                }
+                else
+                {
+                    clicked = true;
+                    GameManger.instance.two = gameObject;
+                    GameManger.instance.Checking = true;
+                    GameManger.instance.Invoke("CheckCard", 1f);
+                    GameManger.instance.turnCounter();
+                }
+            }
+        }
     }
 }
